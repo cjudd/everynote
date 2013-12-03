73d360c2413ecf545dbe137a636141ea8babca9c
Christopher M. Judd <javajudd@gmail.com>
2013-12-02 21:01:12 -0500

    Initial Commit

---
+++ EveryNote/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  EveryNote
+//
+//  Created by Christopher Judd on 12/2/13.
+//  Copyright (c) 2013 Manifest Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MSENAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MSENAppDelegate class]));
+    }
+}
